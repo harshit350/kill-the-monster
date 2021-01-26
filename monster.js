a1689977c1c541b7d683c678c808164352e56396
harshit350 <74818234+harshit350@users.noreply.github.com>
2021-01-26 16:31:38 +0530

    Update monster.js

--- monster.js
+++ monster.js
@@ -8,7 +8,7 @@ class mos{
         this.x=x;
         this.y=y;
         this.r=r;
-        this.image=loadImage("img/Monster-02.png");
+        this.image=loadImage("Monster-02.png");
         this.body=Bodies.circle(this.x,this.y,(this.r)/2,options)
         this.image.scale=0.1
     World.add(world,this.body);
